e63054259cad54dad4768d94634f4b0882e3e68b
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2024-01-20 18:22:21 +0100

    #155 - Frontend is hosted on GitHub Pages
    Changelog explain the process.

--- Frontend/src/app/configuration/config.ts
+++ Frontend/src/app/configuration/config.ts
@@ -1,8 +1,9 @@
+import { environment } from "../../environments/environment";
 import { AuthentificationHelper } from "../authentification/authentification-helper";
 import { LoginToken } from "../login/models/login-token.model";
 
 export class Config {
-    static serverAddress = "http://localhost:5000";
+    static serverAddress = environment.BACKEND_URL;
 
     static httpOptions = function () {
         let authentificationToken: LoginToken = AuthentificationHelper.getLoginToken();
