dca13fe7cb502600ad12d15d0047a086d2ce9a40
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2024-01-27 02:10:08 +0100

    #178 - Roles are available
    The role is now returned when doing the login.

--- Frontend/src/app/login/models/login-request.model.ts
+++ Frontend/src/app/login/models/login-request.model.ts
@@ -1,4 +1,4 @@
 export class LoginRequest {
-    username: string = 'from.hearty@edu.fit.ba';
+    username: string = 'from.hearty.doctor@edu.fit.ba';
     password: string = '1234';
 }
--- Service/Services/LoginService.cs
+++ Service/Services/LoginService.cs
@@ -1,12 +1,22 @@
 
 public record LoginRequest(string Username, string Password);
-public record LoginToken(Guid Id, DateTime ValidUntil);
+public enum RoleType { Doctor, Cardiolog, Patient };
+public record LoginToken(Guid Id, DateTime ValidUntil, RoleType RoleType);
 
 public class LoginService
 {
+    private readonly Dictionary<string, RoleType> roleTypeByUsername = new()
+    {
+        { "from.hearty.doctor@edu.fit.ba", RoleType.Doctor },
+        { "from.hearty.cardiolog@edu.fit.ba", RoleType.Cardiolog },
+        { "from.hearty.patient@edu.fit.ba", RoleType.Patient },
+    };
+
     private readonly Dictionary<LoginRequest, LoginToken?> users = new()
     {
-        { new LoginRequest("from.hearty@edu.fit.ba", "1234"), null }
+        { new LoginRequest("from.hearty.doctor@edu.fit.ba", "1234"), null },
+        { new LoginRequest("from.hearty.cardiolog@edu.fit.ba", "1234"), null },
+        { new LoginRequest("from.hearty.patient@edu.fit.ba", "1234"), null }
     };
 
     private readonly Dictionary<Guid, LoginToken> tokens = [];
@@ -16,12 +26,16 @@ public class LoginService
         if (!this.users.TryGetValue(loginRequest, out var loginToken))
             return null;
 
-        if(loginToken?.ValidUntil < DateTime.Now)
+        if (loginToken?.ValidUntil < DateTime.Now)
             this.tokens.Remove(loginToken.Id);
-        
+
         if (loginToken == null || loginToken.ValidUntil < DateTime.Now)
         {
-            loginToken = new LoginToken(Guid.NewGuid(), DateTime.Now.AddHours(12));
+            loginToken = new LoginToken(
+                Guid.NewGuid(),
+                DateTime.Now.AddHours(12),
+                roleTypeByUsername[loginRequest.Username]
+            );
             this.users[loginRequest] = loginToken;
             this.tokens.Add(loginToken.Id, loginToken);
         }
@@ -29,10 +43,11 @@ public class LoginService
         return loginToken;
     }
 
-    public bool isTokenValid(Guid loginId) {
-        if(!this.tokens.TryGetValue(loginId, out var loginToken))
+    public bool isTokenValid(Guid loginId)
+    {
+        if (!this.tokens.TryGetValue(loginId, out var loginToken))
             return false;
-        if(loginToken.ValidUntil < DateTime.Now)
+        if (loginToken.ValidUntil < DateTime.Now)
             return false;
         return true;
     }
