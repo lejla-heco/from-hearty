0aa6ee4648e5c4f66601771cfbe8ca1f98a3d46f
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2024-02-09 20:48:12 +0100

    localStorage -> sessionStorage

--- Frontend/src/app/authentification/authentification-helper.ts
+++ Frontend/src/app/authentification/authentification-helper.ts
@@ -3,18 +3,18 @@ import { LoginToken } from "../login/models/login-token.model";
 export class AuthentificationHelper {
   
   static isLoggedIn(): boolean {
-    const loginTokenFromLS = localStorage.getItem("Authorization");
+    const loginTokenFromLS = sessionStorage.getItem("Authorization");
     return loginTokenFromLS !== null;
   }
 
   static setLoginToken(loginToken: LoginToken): void {
     if (loginToken == null)
       loginToken = new LoginToken();
-    localStorage.setItem("Authorization", JSON.stringify(loginToken));
+      sessionStorage.setItem("Authorization", JSON.stringify(loginToken));
   }
 
   static getLoginToken(): LoginToken {
-    let loginTokenFromLS = localStorage.getItem("Authorization");
+    let loginTokenFromLS = sessionStorage.getItem("Authorization");
     if (loginTokenFromLS == null)
       return new LoginToken();
 
@@ -28,6 +28,6 @@ export class AuthentificationHelper {
   }
 
   static clearLocalStorage() {
-    localStorage.removeItem("Authorization");
+    sessionStorage.removeItem("Authorization");
   }
 }
